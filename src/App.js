d7e02f640ea16527cb665f96eadcc83a20d0a483
veveethan <veveethan.it22@bitsathy.ac.in>
2025-05-02 18:42:49 +0530

    jbkh

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path="/my-portfolio" element={<Home/>}/>
+      <Route path="/my-portfolo" element={<Home/>}/>
       <Route  path="/" element={<Home/>}/>
       <Route  path="/Project" element={<Project/>}/>
       <Route  path="/About" element={<About/>}/>
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='/my-profile'>
+  <BrowserRouter basename='/my-portfolo'>
     <App />
   </BrowserRouter>
 );
